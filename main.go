9a18931488e31a9820b60e31647dc95deab78313
kinglin <lince110@163.com>
2015-03-19 15:59:13 +0800

    operation:connect,insert,search one item

--- main.go
+++ main.go
@@ -1,10 +1,43 @@
-// demoDBOperation project main.go
 package main
 
 import (
 	"fmt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"log"
 )
 
+type Person struct {
+	Name string
+	Age  string
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	session, err := mgo.Dial("127.0.0.1:27017")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+
+	//拿到需要使用的集合
+	collection := session.DB("test").C("person")
+
+	//插入数据
+	err = collection.Insert(&Person{"aaa", "1"},
+		&Person{"bbb", "2"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//查询单个数据
+	result := Person{}
+	err = collection.Find(bson.M{"name": "aaa"}).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("age:", result.Age)
 }
